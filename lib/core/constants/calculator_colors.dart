8523c3f82b7296e4ceb1b275615deed4bae5c3c4
Abdelrahman <abdot9674@gmail.com>
2026-05-04 02:21:13 +0300

    feat: add calculator color constants

---
+++ lib/core/constants/calculator_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CalculatorColors {
+  static const Color primaryOrange = Color(0xFFFF9800);
+  static const Color buttonLight = Color(0xFFFFFFFF);
+  static const Color buttonDark = Color(0xFFEEEEEE);
+  static const Color textDark = Color(0xFF212121);
+  static const Color textGray = Color(0xFF9E9E9E);
+  static const Color background = Color(0xFFF5F5F5);
+  static const Color displayBackground = Colors.white;
+}
